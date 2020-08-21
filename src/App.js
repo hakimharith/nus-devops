294312c1604d53bca9d68e3b16d9b9ccacbedfbb
hakimharith <36837865+hakimharith@users.noreply.github.com>
2020-08-21 09:58:52 +0800

    AFTER

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          BEFORE <code>src/App.js</code> and save to reload.
+          AFTER <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
